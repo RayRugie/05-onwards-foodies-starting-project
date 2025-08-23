373bd855d76f6460702d6363dbce4232e1df7196
Omorugie Oyibotha <66238145+RayRugie@users.noreply.github.com>
2025-08-23 06:34:41 +0100

    Next.js learning

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealsDetailsPage(){
+    return (
+        <div>
+            <h1>Meal Details Page</h1>
+        </div>
+    )
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,8 @@
+export default function ShareMealsPage() {
+  return (
+    <div>
+      <h1>Share Meals Page</h1>
+      <p>Welcome to the share meals page!</p>
+    </div>
+  );
+}
